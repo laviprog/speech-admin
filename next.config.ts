e42b0f71f2f7ab1e5f1c0b642ff0d3161c692803
laviprog <laviprog@ya.ru>
2025-11-03 13:08:54 +0300

    Update configs

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/speech/admin',
-  assetPrefix: '/speech/admin',
+  basePath: '/speech-admin',
+  assetPrefix: '/speech-admin',
 };
 
 export default nextConfig;
